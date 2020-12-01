ca86c7200ed815d2ee8db79a65a814df423780c8
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-12-01 17:20:16 -0500

    completed first exercise w mult solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+     const arr = str.split('');
+     arr.reverse();
+     return arr.join('');
+     // return str.split('').reverse().join('')
+    
+}
+
+// solution 2
+// function reverse(str) {
+//      let reversed = '' ;
+
+//      for (let character of str) {
+//          reversed = character + reversed 
+//      }
+
+//      return reversed 
+// }
+
+// solution 3
+// function reverse(str) {
+// return str.split('').reduce((reversed, character) => {
+//      return character + reversed; 
+// }, '')
+
+// // return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
 
 module.exports = reverse;
