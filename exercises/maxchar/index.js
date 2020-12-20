bf102ed14b5c0b3412ee5234111d77d022a055ec
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-12-20 13:55:17 -0500

    max char complete with solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// also applies to does string A have same char as string b (is it anagram)
+// convert key (characters) to obj and sort through that map
+// character map- obj where take all char from source String, add to key to obj and value to key is # of times char found in string
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    // FOR IN used to iter over obj
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
